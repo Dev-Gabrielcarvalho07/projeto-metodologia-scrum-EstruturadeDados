2a07a2ecddfd30fd0e13cdae3aeb83d8ac30f324
Joel Felipe dos Santos <dossantosjoelfelipe@gmail.com>
2026-05-05 21:25:59 -0300

    Adicionando BubbleSortService.java - Equipe 4

---
+++ service/BubbleSortService.java
@@ -0,0 +1,20 @@
+package service;
+
+import model.Produto;
+
+public class BubbleSortService {
+
+    public static void bubbleSort(Produto[] lista) {
+        int n = lista.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (lista[j].id > lista[j + 1].id) {
+                    Produto temp = lista[j];
+                    lista[j] = lista[j + 1];
+                    lista[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
